e1ca19bfee9302e089372b24086a39ebdae21e63
martijnd <martijn.dorsman@gmail.com>
2020-07-28 00:02:53 +0200

    Add tests

--- tests/Feature/UsersTest.php
+++ tests/Feature/UsersTest.php
@@ -4,9 +4,21 @@ namespace Tests\Feature;
 
 use App\User;
 use function Pest\Laravel\get;
+use function Pest\Laravel\post;
 
 it('retrieves the user', function () {
     $user = factory(User::class)->create();
 
     get('api/users/'.$user->id)->assertOk();
+});
+
+it('stores the user', function() {
+    $user = factory(User::class)->make();
+
+    post('/api/users', $user->getAttributes())
+        ->assertStatus(201)
+        ->assertJson([
+            'name' => $user->name,
+            'email' => $user->email
+        ]);
 });
