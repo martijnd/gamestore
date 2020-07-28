740237fd5f2f87f5be4265744ade7ecfeb5c9d6f
martijnd <martijn.dorsman@gmail.com>
2020-07-28 22:42:56 +0200

    Add relations, add Game factory

--- app/Game.php
+++ app/Game.php
@@ -3,15 +3,43 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Game extends Model
 {
     protected $fillable = [
         'title',
-        'category_id',
+        'genre_id',
         'company_id',
         'publisher_id',
         'release_date',
         'rating',
     ];
+
+    /**
+     * Define the genre relation.
+     * @return BelongsTo
+     */
+    public function genre()
+    {
+        return $this->belongsTo('App/Genre');
+    }
+
+    /**
+     * Define the company relation.
+     * @return BelongsTo
+     */
+    public function company()
+    {
+        return $this->belongsTo('App/Company');
+    }
+
+    /**
+     * Define the publisher relation.
+     * @return BelongsTo
+     */
+    public function publisher()
+    {
+        return $this->belongsTo('App/Publisher');
+    }
 }
