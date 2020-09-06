856db3939ffbe08f2da666cc3ccd342f870de4dd
Martijn Dorsman <martijn.dorsman@gmail.com>
2020-09-06 18:23:19 +0200

    Add pagination to view

--- src/resources/views/games/index.blade.php
+++ src/resources/views/games/index.blade.php
@@ -1,8 +1,6 @@
 <x-layout>
 
     <div class="container mx-auto p-8">
-
         @livewire('games-list')
-
     </div>
 </x-layout>
--- src/resources/views/livewire/games-list.blade.php
+++ src/resources/views/livewire/games-list.blade.php
@@ -1,41 +1,45 @@
 <div>
     <div class="flex justify-between">
-    <input
-        class="form-input w-1/2"
-        wire:model="search"
-        type="text"
-        placeholder="search"
-    >
-    <a class="bg-purple-500 text-white font-bold rounded shadow py-2 px-4"
-       href="{{ route('games.create') }}">Create game</a>
+        <input
+            class="form-input w-1/2"
+            wire:model="search"
+            type="text"
+            placeholder="search"
+        >
+        <a class="bg-purple-500 text-white font-bold rounded shadow py-2 px-4"
+           href="{{ route('games.create') }}">Create game</a>
     </div>
     <div class="flex mt-4">
         {{ $games->total() }} results
     </div>
-    <table class="table-auto">
+
+    <table class="table-auto mb-4">
         <thead>
-            <tr>
-                <th class="px-4 py-2">Name</th>
-                <th class="px-4 py-2">Genre</th>
-                <th class="px-4 py-2">Company</th>
-                <th class="px-4 py-2">Publisher</th>
-                <th class="px-4 py-2">Release date</th>
-                <th class="px-4 py-2">Actions</th>
-            </tr>
+        <tr>
+            <th class="px-4 py-2">Name</th>
+            <th class="px-4 py-2">Genre</th>
+            <th class="px-4 py-2">Company</th>
+            <th class="px-4 py-2">Publisher</th>
+            <th class="px-4 py-2">Release date</th>
+            <th class="px-4 py-2">Actions</th>
+        </tr>
         </thead>
         <tbody>
-            @foreach($games as $game)
-                <tr @if($loop->even) class="bg-gray-100" @endif }}>
-                    <td class="border px-4 py-2">{{ $game->name }}</td>
-                    <td class="border px-4 py-2">{{ $game->genre->name }}</td>
-                    <td class="border px-4 py-2">{{ $game->company->name }}</td>
-                    <td class="border px-4 py-2">{{ $game->publisher->name }}</td>
-                    <td class="border px-4 py-2">{{ \Carbon\Carbon::parse($game->released_at)->diffForHumans() }}</td>
-                    <td class="border px-4 py-2">
-                        <button wire:click="delete({{ $game->id }})" class="text-red-400 font-bold">Delete</button>
-                    </td>
-                </tr>
-            @endforeach
+        @foreach($games as $game)
+            <tr @if($loop->even) class="bg-gray-100" @endif }}>
+                <td class="border px-4 py-2">{{ $game->name }}</td>
+                <td class="border px-4 py-2">{{ $game->genre->name }}</td>
+                <td class="border px-4 py-2">{{ $game->company->name }}</td>
+                <td class="border px-4 py-2">{{ $game->publisher->name }}</td>
+                <td class="border px-4 py-2">{{ \Carbon\Carbon::parse($game->released_at)->diffForHumans() }}</td>
+                <td class="border px-4 py-2">
+                    <button wire:click="delete({{ $game->id }})" class="text-red-400 font-bold">Delete</button>
+                </td>
+            </tr>
+        @endforeach
         </tbody>
     </table>
+    <div class="p-4">
+        {{ $games->links() }}
+    </div>
 </div>
